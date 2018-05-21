d91df16bd5ad0199604bc2455cd890475fd76e3e
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-05-21 22:44:30 +0300

    Basics

---
+++ basic.py
@@ -0,0 +1,26 @@
+# C0omment
+
+'''
+   This is a multi line
+   Comment
+
+'''
+
+#Single Quotes
+print('Hello World')
+
+#Double Quotes
+print("Hello Wolrd")
+
+# Triple Quotes
+print('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non posuere nulla. Nam at quam velit. Maecenas in gravida elit. Nulla facilisi. Donec sit amet justo sapien. Suspendisse viverra iaculis tortor, ut tempor orci pharetra in. Cras a purus condimentum, facilisis dolor ut, hendrerit ligula. Duis vehicula neque nisl, vel gravida mauris viverra non. Ut porttitor blandit luctus.''')
+
+print('Hello'[0:3])
+print(2)
+
+print(1,2,3,'Hello')
+
+print('LINE1\nLINE2\nLINE3')
+
+print('C:\\somewhere')
+print(r'C:\\somewhere\n')
---
+++ variables.py
@@ -0,0 +1,25 @@
+# Variables and Data Types
+
+greeting='Hello World'
+greeting='Hello Everyone' #reassign
+
+print(greeting)
+
+# Data Types
+myStr='Hello'
+myInt=23
+myFloat=1.4
+myList=[1,2,3,'Hello']
+myDict={'a': 1, 'b': 2, 'C': 3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
+print(myList[3])
+print(myDict['a'])
+print(myStr, 'World')
+
+greeting = myStr + ' World'
+print(greeting)
